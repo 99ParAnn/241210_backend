6771caa13c8e50093a3db71401c7d1738970a335
Petrik Diak <diak@petrik.hu>
2024-12-10 16:45:01 +0100

    Little progress, GET happens, POST seems buggy

--- src/children/dto/create-child.dto.ts
+++ src/children/dto/create-child.dto.ts
@@ -1,1 +1,22 @@
-export class CreateChildDto {}
+
+import { IsBoolean, IsDateString, isNotEmpty, IsNotEmpty, isString, IsString } from "class-validator";
+
+export class CreateChildDto {
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  wasGood: boolean;
+
+  @IsString()
+  wantsToy: string;
+}
+
+
